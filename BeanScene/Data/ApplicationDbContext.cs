0891ba6ccd0b89870eb8c7a8556469f891208fbd
Ramazan Carpan <ramazan.carpan@outlook.com>
2024-10-24 10:33:53 +1100

    created project

---
+++ BeanScene/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BeanScean.Models; 
+
+namespace BeanScean.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestaurantArea> RestaurantAreas { get; set; }
+        public DbSet<RestaurantTable> RestaurantTables { get; set; }
+        public DbSet<Sitting> Sittings { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+       // public DbSet<AspNetUsers> Users { get; set; }  // ths come from  login system 
+        //public DbSet<AspNetRoles> Roles { get; set; } // ths come from  login system 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+         
+        }
+    }
+}
