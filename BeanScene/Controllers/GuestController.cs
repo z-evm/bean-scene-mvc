cd163cfdb7618c11cfe62a5aa73f0cf5567ede27
Alexander Maniago <alexandermaniago@gmail.com>
2024-10-25 10:43:04 +1100

    Created Controllers for Guests and Staff

---
+++ BeanScene/Controllers/GuestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeanScene.Controllers
+{
+    public class GuestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
