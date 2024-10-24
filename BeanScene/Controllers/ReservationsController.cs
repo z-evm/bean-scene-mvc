1d87c874a974f47f42dbf20919f0b11e108be42a
Alexander Maniago <alexandermaniago@gmail.com>
2024-10-25 10:28:06 +1100

    Created Controllers for the Menu, Reservations, Tables and Orders

---
+++ BeanScene/Controllers/ReservationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeanScene.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
