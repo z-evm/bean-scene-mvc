865b8cb57424e45cb3869ac7ae3957760d57ddb0
Alexander Maniago <alexandermaniago@gmail.com>
2024-10-26 15:19:34 +1100

    Created Areas for the Admin (Manager), Staff and Members of Bean Scene

---
+++ BeanScene/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeanScene.Areas.Admin.Controllers;
+[Area("Admin")]
+public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
---
+++ BeanScene/Areas/Staff/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeanScene.Areas.Staff.Controllers
+{
+    [Area("Staff")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
