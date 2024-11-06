263d6ac2c35ccf32f709517a20436d86e8df586d
Alexander Maniago <alexandermaniago@gmail.com>
2024-11-06 13:06:34 +1100

    Made sure that all pages required are created for the admin (manager), staff and member users

---
+++ BeanScene/Areas/Staff/Controllers/EditreservationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeanScene.Areas.Staff.Controllers
+{
+    public class EditreservationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
